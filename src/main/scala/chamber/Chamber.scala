1ba715799a3c86debadcec1aa533d9492b9cf2fa
Oscar De Alva <oscar.ivan98@gmail.com>
2020-10-09 11:23:27 -0500

    Added more documentation for Chamber class

--- src/main/scala/chamber/Chamber.scala
+++ src/main/scala/chamber/Chamber.scala
@@ -19,91 +19,108 @@ class Chamber private (private val init: String) {
   /**
     * Returns true if the given letter can be animated ('R' or 'L').
     *
-    * @param letter
-    * @return
+    * @param letter Character to validate
+    * @return if the given letter is a particle
     */
-  private def isValidLetter(letter: Char): Boolean = letter == 'R' || letter == 'L'
+  private def isValidParticle(letter: Char): Boolean = letter == 'R' || letter == 'L'
 
   /**
     * Returns the animation operation depending on the given particle.
     *
-    * @param letter Current letter of the animation
-    * @param curr Current index of the letter
+    * @param particle Current particle of the animation
+    * @param curr Current index of the particle
     * @param speed Speed of the animation
     * @return A function that correctly calculates the new position of the given particle.
     */
-  private def animateOperation(letter: Char): (Int, Int) => Int =
-    if (letter == 'L')
+  private def animateOperation(particle: Char): (Int, Int) => Int =
+    if (particle == 'L')
       (curr: Int, speed: Int) => curr - speed
     else
       (curr: Int, speed: Int) => curr + speed
 
   /**
-    * Determines if the position can be inside the Init string.
+    * Returns true if the position given is inside the Chamber's space.
     *
-    * Returns true if the position given is inside the Init string
     * @param pos Position to evaluate
-    * @return
+    * @return if the position is inside the Init string
     */
   private def isValidPosition(pos: Int): Boolean = pos >= 0 && pos < init.length
 
   /**
-    * Return all the valid letters for the animation and their postion inside
+    * Return all the valid particles for the animation and their postion inside
     * the initial condition.
     *
-    * @return A List of valid letters in the inital condition
+    * @return A List of valid particles in the inital condition
     */
-  private def getValidLetters(): List[(Char, Int)] =
-    init.zipWithIndex.filter { case (letter, _) => isValidLetter(letter) }.toList
+  private def getParticles(): List[(Char, Int)] =
+    init.zipWithIndex.filter { case (letter, _) => isValidParticle(letter) }.toList
+
+  /**
+    * Returns the initial conditions given to the Chamber as an
+    * animation step.
+    *
+    * @return A String representing the initial conditions as an animation
+    */
+  private def generateInitialStep(): String = init.map(char => if (isValidParticle(char)) 'X' else '.')
 
   /**
     * Returns the animation process starting from the class' intial
     * condition, determined by the given speed.
     *
-    * @param speed
+    * @param speed the Speed for each step of the animation
     * @return An Array containing each step of the animation as a String, where
     *   each 'X' determines a particle.
     */
   def animate(speed: Int): Array[String] = {
     require(speed > 0 && speed <= 10, "speed must be between 1 and 10.")
 
-    // Filter out only the letters and their initial position from init
-    val letters = getValidLetters()
+    // Filter out only the particles and their initial position from init
+    val particles = getParticles()
 
-    val initialState = init.map(char => if (isValidLetter(char)) 'X' else '.') :: Nil
+    val initialState = generateInitialStep() :: Nil
 
     @tailrec
-    def loop(stepLetters: List[(Char, Int)], animations: List[String]): List[String] = {
-      // Base case: All letters have been animated succesfully
-      if (stepLetters.isEmpty) animations // Return calculated animations
+    def loop(stepParticles: List[(Char, Int)], animations: List[String]): List[String] = {
+      // Base case: All particles have been animated succesfully
+      if (stepParticles.isEmpty) animations // Return calculated animations
       else {
-        // Intial step for the animation
-        val step         = new StringBuilder(EMPTY_ANIMATION)
-        val validLetters = ListBuffer.empty[(Char, Int)]
+        // Generate step for the animation
 
-        for ((letter, currentPosition) <- stepLetters) {
-          // Get the correct operation for the current letter
-          val operation = animateOperation(letter)
-          // Calculate new position
-          val newPosition = operation(currentPosition, speed)
+        /*
+          Start with empty list of particles and empty animation step.
+          Fill the list with valid particles, along with their new position, that
+          will be used for the next step and animate this step.
+            - A valid particle is one that is still inside the chamber's space.
+         */
+        val (validParticles, animation) =
+          stepParticles.foldRight((List.empty[(Char, Int)], EMPTY_ANIMATION)) {
+            case ((particle, currentPosition), (validParticles, animation)) =>
+              // Get the correct operation for the current particle
+              val operation = animateOperation(particle)
+              // Calculate new position
+              val newPosition = operation(currentPosition, speed)
 
-          if (isValidPosition(newPosition)) { // New position is inbounds of Init
-            // Do nothing with the step if there's already a particle
-            if (step(newPosition) != 'X')
-              step(newPosition) = 'X'
+              if (isValidPosition(newPosition)) { // New position is inbounds of initial space
+                // Do nothing with the step if there's already a particle
+                val stepAnimation =
+                  if (animation(newPosition) != 'X')
+                    animation.updated(newPosition, 'X')
+                  else animation
 
-            // Add the letter and its new possition as a valid letter for next iteration
-            validLetters += (letter -> newPosition)
+                // Add the particle and its new possition as a valid particle for next iteration
+                val newValidParticles = (particle, newPosition) :: validParticles
+
+                (newValidParticles, stepAnimation)
+              } else (validParticles, animation) // Don't add particle for next iteration
           }
-        }
 
-        // Call recursion with valid letters and adding the animated step to the animations
-        loop(validLetters.toList, step.toString :: animations)
+        // Call recursion with valid particles and adding the animated step to the animations
+        loop(validParticles, animation :: animations)
       }
     }
 
     // Convert animations into array and return them
-    loop(letters, initialState).reverse.toArray
+    loop(particles, initialState).reverse.toArray
   }
 }
 
